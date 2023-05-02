8eea9758e0afd3c1d4067b23b8476bc971fc1e6b
cybergaz <kkanttechy@gmail.com>
2023-05-02 13:38:19 +0530

    ....

--- .config/hypr/scripts/lid_down.sh
+++ .config/hypr/scripts/lid_down.sh
@@ -1,13 +1,12 @@
-if [ -f /tmp/lid_script_check_down ]
-then
-    hyprctl dispatch dpms off
-    $HOME/.config/waybar/scripts/caffeine.sh
+if [ -f /tmp/lid_script_check_down ]; then
+	hyprctl dispatch dpms off
+	# $HOME/.config/waybar/scripts/caffeine.sh
 #     rm -rf /tmp/hyprctl_log
 #     hyprctl clients | grep Window >> /tmp/hyprctl_log
 #
 #     kill $(hyprctl clients | grep pid | awk '{ print $2 }' ) &
 else
-    touch /tmp/lid_script_check_down
-    # notify-send "scripts not executed don't worry"
+	touch /tmp/lid_script_check_down
+	# notify-send "scripts not executed don't worry"
 
 fi
--- .config/hypr/scripts/sleep.sh
+++ .config/hypr/scripts/sleep.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 swayidle -w \
-                timeout 120 'temp=$(brightnessctl g); brightnessctl set $((temp / 2))' \
-                    resume 'temp=$(brightnessctl g); brightnessctl set $((temp * 2))' \
-                timeout 300 "$HOME/.config/hypr/scripts/lock.sh & sleep 2 && hyprctl dispatch dpms off" \
-                    resume 'hyprctl dispatch dpms on' \
-                
-
-
-#Matcha-dark-sea
+	timeout 60 'temp=$(brightnessctl g); brightnessctl set $((temp / 2))' \
+	resume 'temp=$(brightnessctl g); brightnessctl set $((temp * 2))' \
+	timeout 100 'temp=$(brightnessctl g); brightnessctl set $((temp / 2)) ; alacritty --config-file "$HOME/.config/alacritty/alacritty2.yml" -T alacritty_time -e tty-clock -csC 5' \
+	resume 'temp=$(brightnessctl g); brightnessctl set $((temp * 2))'
+# timeout 20 "hyprctl dispatch dpms off" \
+# resume '$HOME/.config/hypr/scripts/lid_up.sh'
